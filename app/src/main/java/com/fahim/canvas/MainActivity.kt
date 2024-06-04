e737fb7ee2ec7c5560e3350de049fa57b33b17fa
ifahimkhan <khan.fahim95@hotmail.com>
2024-06-04 22:35:59 +0400

    if applied this commit will add Basic shapes

--- app/src/main/java/com/fahim/canvas/MainActivity.kt
+++ app/src/main/java/com/fahim/canvas/MainActivity.kt
@@ -3,12 +3,14 @@ package com.fahim.canvas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.tooling.preview.Preview
 import com.fahim.canvas.ui.theme.CanvasTheme
 
@@ -16,31 +18,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CanvasTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyCanvas()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CanvasTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/fahim/canvas/Shapes.kt
@@ -0,0 +1,67 @@
+package com.fahim.canvas
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun MyCanvas() {
+    Canvas(
+        modifier = Modifier
+            .padding(20.dp)
+            .size(300.dp)
+    ) {
+        drawRect(
+            color = Color.Black,
+            size = size
+        )
+        drawRect(
+            color = Color.Red,
+            size = Size(100f, 100f),
+            style = Stroke(width = 3.dp.toPx()),
+            topLeft = Offset(100f, 100f)
+        )
+        drawCircle(
+            brush = Brush.radialGradient(
+                colors = listOf(Color.Red, Color.Yellow),
+                radius = 100f,
+                center = center
+            ),
+            radius = 100f,
+        )
+        drawArc(
+            color = Color.Green,
+            startAngle = 0f,
+            sweepAngle = 300f,
+            useCenter = true,
+            topLeft = Offset(100f, 500f),
+            size = Size(200f, 200f),
+            style = Stroke(
+                width = 3.dp.toPx()
+            )
+        )
+        drawOval(
+            color = Color.Magenta,
+            topLeft = Offset(500f, 100f),
+            size = Size(200f, 300f)
+        )
+        drawLine(
+            color = Color.Cyan,
+            start = Offset(700f, 700f),
+            end = Offset(700f, 700f),
+            strokeWidth = 5.dp.toPx()
+        )
+
+
+    }
+}
