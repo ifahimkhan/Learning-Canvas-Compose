0b2986bc38d97e9793c7eac1cd7fab2e25c38aa7
ifahimkhan <khan.fahim95@hotmail.com>
2024-06-07 21:32:35 +0400

    TouchScreen will start

--- app/src/main/java/com/fahim/canvas/MainActivity.kt
+++ app/src/main/java/com/fahim/canvas/MainActivity.kt
@@ -18,7 +18,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyCanvas()
+//            MyCanvas()
+            TouchScreen()
         }
     }
 }
