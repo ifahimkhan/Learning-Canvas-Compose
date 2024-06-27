93ac26e5860319448b2d7e71ab6b00bd7dac06f0
ifahimkhan <khan.fahim95@hotmail.com>
2024-06-27 06:07:51 +0400

    If applied this commit will add Path Effects

--- app/src/main/java/com/fahim/canvas/ui/screen/drawPath/PathEffects.kt
+++ app/src/main/java/com/fahim/canvas/ui/screen/drawPath/PathEffects.kt
@@ -5,20 +5,39 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun PathEffects() {
     Canvas(modifier = Modifier.fillMaxSize()) {
-        rotate(45f, pivot = Offset(200f, 200f)) {
+        val circle = Path().apply {
+            addOval(Rect(center = Offset(400f, 400f), radius = 300f))
+        }
+        drawPath(
+            path = circle,
+            color = Color.Black,
+            style = Stroke(width = 5.dp.toPx())
+        )
+        clipPath(
+            path = circle,
+
+
+            ) {
+
             drawRect(
                 color = Color.Red,
-                topLeft = Offset(100f, 100f),
-                size = Size(200f, 200f)
+                topLeft = Offset(400f, 0f),
+                size = Size(400f, 400f)
             )
+
         }
     }
 
